b6c2d5baf048d08eff9df7ed9da356e86c3d4348
Md Abid Sikder <abid@abidsikder.com>
2022-07-24 22:31:40 -0400

    feat: put sites ready to be deployed under a basic website

--- update.sh
+++ update.sh
@@ -12,10 +12,13 @@ cd projects
 for PROJECT in ./*
 do
   cd $PROJECT
-  echo $PROJECT
+  echo "Working on $PROJECT"
   PROJECT_DIR=$(pwd)
   (cd $PROJECT_DIR; ./docs.sh)
-  cp -r docs-site/site ../..
-  cd ../..
-  mv site $(basename $PROJECT)
+  cp -r docs-site/site ../../website
+  cd ../../website
+  NEW_NAME=$(basename $PROJECT)
+  rm -r $NEW_NAME #removes old project
+  mv site $NEW_NAME
+  cd ..
 done
